d14e82950b1d2f0961dde696e794a28509c8289a
hagihara <hagihara@1-10.com>
2024-11-14 20:14:58 +0900

    Update セルの状態管理

--- src/pages/_index.ts
+++ src/pages/_index.ts
@@ -1,5 +1,7 @@
 ;(async () => {
   const GRID_SIZE = 32
+  const UPDATE_INTERVAL = 200
+  let step = 0
 
   const canvas = document.querySelector<HTMLCanvasElement>('canvas')!
 
@@ -78,13 +80,17 @@
       };
       
       @group(0) @binding(0) var<uniform> grid: vec2f;
+      @group(0) @binding(1) var<storage> cellState: array<u32>;
       
       @vertex
       fn vertexMain(input: VertexInput) -> VertexOutput {
         let i = f32(input.instance);
         let cell = vec2f(i % grid.x, floor(i / grid.x));
+        let state = f32(cellState[input.instance]);
+        
         let cellOffset = cell / grid * 2;
-        let gridPos = (input.pos + 1) / grid - 1 + cellOffset;
+        // let gridPos = (input.pos + 1) / grid - 1 + cellOffset;
+        let gridPos = (input.pos*state+1) / grid - 1 + cellOffset;
         
         var output: VertexOutput;
         output.pos = vec4f(gridPos, 0, 1);
@@ -128,38 +134,86 @@
   })
   device.queue.writeBuffer(uniformBuffer, 0, uniformArray)
 
+  // セルの状態を保持するストレージバッファ作成
+  const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE)
+  const cellStateStorage = [
+    device.createBuffer({
+      label: 'Cell State A',
+      size: cellStateArray.byteLength,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    }),
+    device.createBuffer({
+      label: 'Cell State B',
+      size: cellStateArray.byteLength,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    }),
+  ]
+
+  for (let i = 0; i < cellStateArray.length; i += 3) {
+    cellStateArray[i] = 1
+  }
+  device.queue.writeBuffer(cellStateStorage[0], 0, cellStateArray)
+
+  for (let i = 0; i < cellStateArray.length; i++) {
+    cellStateArray[i] = i % 2
+  }
+  device.queue.writeBuffer(cellStateStorage[1], 0, cellStateArray)
+
   // バインドグループ作成
-  const bindGroup = device.createBindGroup({
-    label: 'Cell renderer bind group',
-    layout: cellPipeline.getBindGroupLayout(0),
-    entries: [
-      {
-        binding: 0,
-        resource: { buffer: uniformBuffer },
-      },
-    ],
-  })
+  const bindGroups = [
+    device.createBindGroup({
+      label: 'Cell renderer bind group A',
+      layout: cellPipeline.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 0,
+          resource: { buffer: uniformBuffer },
+        },
+        {
+          binding: 1,
+          resource: { buffer: cellStateStorage[0] },
+        },
+      ],
+    }),
+    device.createBindGroup({
+      label: 'Cell renderer bind group B',
+      layout: cellPipeline.getBindGroupLayout(0),
+      entries: [
+        {
+          binding: 0,
+          resource: { buffer: uniformBuffer },
+        },
+        {
+          binding: 1,
+          resource: { buffer: cellStateStorage[1] },
+        },
+      ],
+    }),
+  ]
 
-  const encoder = device.createCommandEncoder()
-  const pass = encoder.beginRenderPass({
-    colorAttachments: [
-      {
-        view: context.getCurrentTexture().createView(),
-        loadOp: 'clear',
-        clearValue: { r: 0, g: 0, b: 0.4, a: 1 },
-        storeOp: 'store',
-      },
-    ],
-  })
+  function updateGrid() {
+    step += 1
 
-  pass.setPipeline(cellPipeline)
-  pass.setVertexBuffer(0, vertexBuffer)
-  pass.setBindGroup(0, bindGroup)
-  pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE)
+    const encoder = device.createCommandEncoder()
+    const pass = encoder.beginRenderPass({
+      colorAttachments: [
+        {
+          view: context.getCurrentTexture().createView(),
+          loadOp: 'clear',
+          clearValue: { r: 0, g: 0, b: 0.4, a: 1.0 },
+          storeOp: 'store',
+        },
+      ],
+    })
 
-  pass.end()
+    pass.setPipeline(cellPipeline)
+    pass.setBindGroup(0, bindGroups[step % 2]) // Updated!
+    pass.setVertexBuffer(0, vertexBuffer)
+    pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE)
 
-  // const commandBuffer = encoder.finish()
-  // device.queue.submit([commandBuffer])
-  device.queue.submit([encoder.finish()])
+    pass.end()
+    device.queue.submit([encoder.finish()])
+  }
+
+  setInterval(updateGrid, UPDATE_INTERVAL)
 })()
